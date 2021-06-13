d49c5b57a912fdc429081bd9dd57abbfb35ca2cd
Sohee Kim <soheekim.dev@gmail.com>
2021-06-13 22:45:59 +0900

    View, Text, Image, StyleSheet

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+      <Image source={{uri: 'https://randomuser.me/api/portraits/women/1.jpg'}} style={styles.img} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    color: 'darkslateblue',
+    fontSize: 30
+  },
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+  },
+});
 
 export default App;
