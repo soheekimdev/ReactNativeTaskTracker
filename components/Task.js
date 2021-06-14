d79b715e207d4746cb2de706d9cc6110c4d8b358
Sohee Kim <soheekim.dev@gmail.com>
2021-06-14 11:21:15 +0900

    styling: Task.js

--- components/Task.js
+++ components/Task.js
@@ -3,16 +3,29 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const Task = ({ task }) => {
   return (
-    <TouchableOpacity>
-      <View>
-        <Text>{task.text}</Text>
+    <TouchableOpacity style={styles.task}>
+      <View style={styles.taskView}>
+        <Text style={styles.taskText}>{task.text}</Text>
       </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  
+  task: {
+    padding: 15,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  taskView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  taskText: {
+    fontSize: 18,
+  }
 });
 
 export default Task;
